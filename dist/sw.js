ab39b051d77ca89d735d6d8a58a91147b41c5a02
ldexterldesign <github@ldexterldesign.co.ukm>
2019-06-18 16:18:58 +0200

    Create dist

--- dist/sw.js
+++ dist/sw.js
@@ -2,20 +2,20 @@ importScripts('/_nuxt/workbox.4c4f5ca6.js')
 
 workbox.precaching.precacheAndRoute([
   {
-    "url": "/_nuxt/0ff1743a833c498dcc1c.js",
-    "revision": "605e8b8e09c5030e140803fa924586d0"
+    "url": "/_nuxt/18e8191816d941116bb5.js",
+    "revision": "ffc5998f51d5561c402827a78722529f"
   },
   {
     "url": "/_nuxt/53b373af825cbe5a5756.js",
     "revision": "3043ebfe75b1a28df5f731cc18e28d20"
   },
   {
-    "url": "/_nuxt/aa34f89f3696da0dbe57.js",
-    "revision": "06d901d383c393dc303754a865df00f9"
-  },
-  {
     "url": "/_nuxt/f55df5d823bed1f5be4f.js",
     "revision": "73f59412c90e1cef11988c12857e075a"
+  },
+  {
+    "url": "/_nuxt/f9c07714b1659ea943ed.js",
+    "revision": "df8b2e62061050617dbdd98fd39dd999"
   }
 ], {
   "cacheId": "ldexterldesign",
